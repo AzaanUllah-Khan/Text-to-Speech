3405bd7a0caf3a8ff9cda4258bf32f89ab476f30
AzaanUllah-Khan <azaanullahkhan291@gmail.com>
2023-06-11 18:26:23 +0500

    some code ~

---
+++ script.js
@@ -0,0 +1,8 @@
+
+let button = document.querySelector('button');
+let text = document.querySelector('textarea');
+
+button.addEventListener('click', () => {
+    let utterance = new SpeechSynthesisUtterance(text.value);
+    speechSynthesis.speak(utterance);
+});
